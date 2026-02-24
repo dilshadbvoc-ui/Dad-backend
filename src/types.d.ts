06f55b80a3c0265c263d68a3d976da2754f3d7e7
Prohostix <retro@retros-MacBook-Air.local>
2026-02-24 20:18:18 +0530

    fix: disable forceConsistentCasingInFileNames to allow build on Linux

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,2 @@
-
-interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    FB: any;
-    fbAsyncInit: () => void;
-    checkLoginState: () => void;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare let FB: any;
+declare module 'hpp';
+declare module 'zxcvbn';
