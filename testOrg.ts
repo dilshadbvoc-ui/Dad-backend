707d7f5acc4578376a62e05a0d22b25b5233ba28
adithyandileep <adithyandileep815@gmail.com>
2026-07-26 14:22:48 +0530

    docs: remove unused docs and debug scripts
    - Remove obsolete markdown documentation files
    - Remove temporary database diagnostic scripts
    - Clean up workspace root directory

--- testOrg.ts
+++
@@ -1,8 +0,0 @@
-import prisma from './src/config/prisma';
-
-async function main() {
-    const org = await prisma.organisation.findFirst({ select: { shufflerConfig: true } });
-    console.log(JSON.stringify(org, null, 2));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
